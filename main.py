c62ecaa93740730168703b73bea26fbd3722ac41
lAvantGardiste <hamzaoc10@gmail.com>
2026-01-06 00:55:09 +0100

    Football project

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import read_video, save_video
+
+def main():
+    video_frames = read_video("input_video/08fd33_4.mp4")
+    save_video(video_frames, "output_video/output_video.avi")
+
+    
+if __name__ == "__main__":
+    main()
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .video_utils import read_video, save_video
+
+
